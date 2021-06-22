d12aa61e481334e6f72e60903305ea2588ba032a
Jhansi Rani <jhansi@Jhansis-MacBook-Air.local>
2021-06-22 17:45:34 -0400

    hide and show with jquery

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function (){
+  $('#hide').click(function (){
+    $('li').hide()//hides the list
+  });
+  $('#show').click(function (){
+    $('li').show()//show the list
+  });
+});
